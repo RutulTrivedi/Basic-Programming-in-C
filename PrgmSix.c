fd1ef6bb8cddc020560309ab7a34bc039273092e
Rutul Trivedi <114494847+RutulTrivedi@users.noreply.github.com>
2023-07-11 22:03:38 +0530

    C program to print HELLO using pattern in single line.

---
+++ PrgmSix.c
@@ -0,0 +1,81 @@
+//C program to print 'HELLO' pattern using star pattern
+#include<stdio.h>
+
+void main(){
+	int i,j,temp = 5;
+	
+	//Outer loop to print number of rows
+	for(i=0;i<temp;i++){
+		
+		//Nested loop to print character 'H'
+		for(j=0;j<temp;j++){
+			if(j==0 || j==temp-1 || i==temp/2){
+				printf("*");
+			}
+			else{
+				printf(" ");
+			}
+		}
+		
+		//Nested loop to print space between characters
+		for(j=0;j<2;j++){
+			printf(" ");
+		}
+		
+		//Nested loop to print character 'E'
+		for(j=0;j<temp;j++){
+			if(i==0 || i==temp/2 || i==temp-1 || j==0){
+				printf("*");
+			}
+			else{
+				printf(" ");
+			}
+		}
+		
+		//Nested loop to print space between characters
+		for(j=0;j<2;j++){
+			printf(" ");
+		}
+		
+		//Nested loop to print character 'L'
+		for(j=0;j<temp;j++){
+			if(i==temp-1 || j==0){
+				printf("*");
+			}
+			else{
+				printf(" ");
+			}
+		}
+		
+		//Nested loop to print space between characters
+		for(j=0;j<2;j++){
+			printf(" ");
+		}
+		
+		//Nested loop to print character 'L'
+		for(j=0;j<temp;j++){
+			if(i==temp-1 || j==0){
+				printf("*");
+			}
+			else{
+				printf(" ");
+			}
+		}
+		
+		//Nested loop to print space between characters
+		for(j=0;j<2;j++){
+			printf(" ");
+		}
+		
+		//Nested loop to print character 'O'
+		for(j=0;j<temp;j++){
+			if(i==0 || i==temp-1 || j==0 || j==temp-1){
+				printf("*");
+			}
+			else{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+}
